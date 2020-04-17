3594d153328d678be2c15b06407254a561b1aa92
LajmiMohamed <mohamedlajmi198@gmail.com>
2020-04-17 23:18:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import {v4 as uuidv4} from "uuid";
 function App() {
+  const [selectedTodo,setSelectedTodo]=useState({})
+  const [editMode,seteditMode]=useState(false)
+  const [newTodo,setnewTodo]=useState('')
+  const [todos,setTodos]=useState([
+
+  ])
+  const add =()=>{
+     setTodos(todos.concat({id:uuidv4(),text:newTodo})) 
+     setnewTodo("")
+  }
+  const deleteTodo=(id)=>{
+    setTodos(todos.filter(x=>x.id !== id))
+  }
+  const edit=()=>{
+    setTodos(todos.map(el=>el.id===selectedTodo.id ? selectedTodo : el))
+    seteditMode(false)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { editMode ? 
+        (
+          <div>
+          <input type="text" onChange={e=>setSelectedTodo({...selectedTodo , text:e.target.value })} value={selectedTodo.text}/>
+          <button onClick={edit}>Edit Todo</button>
+        </div> 
+        ):
+          <div>
+          <input type="text" onChange={e=>setnewTodo(e.target.value) } value={newTodo}/>
+          <button onClick={add}>Add</button>
+        </div>}
+        {todos.map(el=> (
+        <div style={{display:'flex',alignItems:"center"}}>
+          <h3>{el.text}</h3>
+          <button onClick={()=> deleteTodo(el.id)}>Delete</button>
+          <button onClick={()=>{
+           setSelectedTodo(el)
+            seteditMode(true)}}>Edit</button>
+        </div>
+        )
+        )
+        }
       </header>
     </div>
   );
